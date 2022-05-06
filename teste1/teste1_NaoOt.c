6c960f385c2852e2d204cdad16d51ea8efc85b87
LeonardoGuths <ldsguths@inf.ufpel.edu.br>
2022-05-06 17:21:49 -0300

    teste1 added

---
+++ teste1/teste1_NaoOt.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <time.h>
+
+int somaMatriz (int x, int y);
+
+int main ()
+{
+    clock_t tempo;
+    int matriz[500][500], i, j, iteracoes=10000, x;
+    double tempo_total = 0.0;
+
+    for (i=0; i<500; i++)
+    {
+        for (j=0; j<500; j++)
+        {
+            matriz[i][j] = i+j;
+        }
+    }
+
+    for (x=0; x<iteracoes; x++)
+    {
+        for (i=0; i<500; i++)
+        {
+        tempo = clock();
+        for (j=0; j<500; j++)
+        {
+            somaMatriz(matriz[i][j], matriz[j][i]);
+        }
+        tempo = clock() - tempo;
+        tempo_total += ((double)tempo)/CLOCKS_PER_SEC;
+        }
+    }
+    
+    printf ("tempo medio: %lf\n",tempo_total/iteracoes);
+
+}
+
+int somaMatriz (int x, int y)
+{
+    x = x+y;
+    x = x+4.0;
+    x = x+6.0;
+    return x;
+}
---
+++ teste1/teste1_Ot.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+
+int main ()
+{
+    clock_t tempo;
+    int matriz[500][500], i, j, iteracoes=10000, x;
+    double tempo_total = 0.0;
+
+    for (i=0; i<500; i++)
+    {
+        for (j=0; j<500; j++)
+        {
+            matriz[i][j] = i+j;
+        }
+    }
+
+    for (x=0; x<iteracoes; x++)
+    {
+        for (i=0; i<500; i++)
+        {
+        tempo = clock();
+        for (j=0; j<500; j++)
+        {
+            matriz[i][j] = matriz[i][j] + matriz[j][i] + 4 + 6;
+        }
+        tempo = clock() - tempo;
+        tempo_total += ((double)tempo)/CLOCKS_PER_SEC;
+        }
+    }
+    
+    printf ("tempo medio: %lf\n",tempo_total/iteracoes);
+
+}
